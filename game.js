c3fa93528b1fb5974c1f450599252955531b4e3f
Matt Mahowald <10021147+mattmahowald@users.noreply.github.com>
2022-05-23 22:38:24 +0000

    lit

--- game.js
+++ game.js
@@ -1,29 +1,227 @@
-var NUM_SQUARES = 6;
-var NUM_ROWS = 4;
+//for js button
+/*
+const easyButton = document.getElementById("Easybutton");
+
+easyButton.addEventListener("click", function() {
+  
+});
+*/
+
+var music;
+var NUM_SQUARES;
+var NUM_ROWS;
 var PADDING = 10;
-var FONT = "24pt Comic Sans MS"
+var FONT = "28pt Monospace";
 var width = 400;
 var height = 500;
-var length = (width - PADDING) / NUM_SQUARES - PADDING;
+
 var arrGreen = [96, 181, 100];
+var arrGreenShadow = [30, 138, 32];
 var arrYellow = [224, 198, 25];
 var arrRed = [242, 10, 10];
 
-var col = 0;
+var length;
+var squareX = PADDING;
+var squareY = PADDING;
+
+var label;
 var curLabels = [];
+
 var shift = false;
 
 var guess = "";
-var answer = "2x5=10";
 
-var label;
+var getAnswer = true;
+var answer;
 
-function drawLabel(text) {
+//win screen
+var blackscreen;
+var winText;
+var statsButton;
+var winScreenON = false;
+
+//stats screen
+var statsScreenON = false;
+var statsBack;
+var summary = [];
+
+var valid;
+
+var guessCount;
+
+//menu
+var menuBool = true;
+var menuText;
+var easyPlay;
+var normalButton;
+var normalShadow;
+var hardButton;
+var hardShadow;
+
+//incorrect screen
+var incoBack;
+
+var cheat = false;
+
+function menu() {
+
+    normalShadow = new Rectangle(getWidth() / 2 + 25, getHeight() / 8 + 25);
+    normalShadow.setPosition(
+        getWidth() / 2 - normalShadow.getWidth() / 2,
+        getHeight() / 2 - normalShadow.getHeight() / 2
+    );
+    normalShadow.setColor(Color.WHITE);
+
+    add(normalShadow);
+
+    normalButton = new Rectangle(getWidth() / 2, getHeight() / 8);
+    normalButton.setPosition(
+        getWidth() / 2 - normalButton.getWidth() / 2,
+        getHeight() / 2 - normalButton.getHeight() / 2
+    );
+    normalButton.setColor(Color.grey);
+
+    add(normalButton);
+
+    easyText = drawText(normalButton.getX() + normalButton.getWidth() / 2,
+        normalButton.getY() + normalButton.getHeight() / 2, "PLAY", Color.WHITE, FONT);
+    /*
+        hardButton = new Rectangle(getWidth()/2, getHeight()/8);
+        hardButton.setPosition(
+          getWidth()/2 - hardButton.getWidth()/2,
+          getHeight()/2 - hardButton.getHeight()/2 + 100);
+        add(hardButton);
+    */
+}
+
+function move(i) {
+    if (menuBool) {
+        if (easyText.containsPoint(i.getX(), i.getY()) == false) {
+            if (normalButton.containsPoint(i.getX(), i.getY())) {
+                normalShadow.setColor(setColor(arrGreenShadow));
+                normalButton.setColor(setSquareColor(i.getX(), i.getY(), setColor(arrGreen)));
+            } else {
+                normalShadow.setColor(Color.WHITE);
+                normalButton.setColor(Color.grey);
+            }
+        }
+    }
+}
+
+function winScreen() {
+    blackscreen = new Rectangle(getWidth(), getHeight());
+    blackscreen.setPosition(0, 0);
+    blackscreen.setColor(Color.BLACK);
+    statsButton = new Rectangle(getWidth() / 2, getHeight() / 8);
+    statsButton.setPosition(
+        getWidth() / 2 - statsButton.getWidth() / 2,
+        getHeight() / 2 - statsButton.getHeight() / 2 + 100);
+    statsButton.setColor(Color.WHITE);
+}
+
+function statsScreen() {
+    statsBack = new Rectangle(getWidth(), getHeight());
+    statsBack.setPosition(0, 0);
+    statsBack.setColor(Color.WHITE);
+}
+
+function start() {
+    menu();
+    if (menuBool) {
+        mouseMoveMethod(move);
+    }
+    winScreen();
+    statsScreen();
+    mouseClickMethod(click);
+}
+
+function drawTiles() {
+    for (var row = 0; row < NUM_ROWS; row++) {
+        for (var col = 0; col < NUM_SQUARES; col++) {
+            drawSquare(squareX, squareY, length, length, getColor());
+            squareX += length + PADDING;
+        }
+        squareX = PADDING;
+        squareY += length + PADDING;
+    }
+    squareX = PADDING;
+    squareY = PADDING;
+}
+
+var sumArri = 0;
+function getColor(){
+    if(statsScreenON){
+        if (summary[sumArri] == "GREEN"){
+            sumArri+=1;
+            return setColor(arrGreen);
+        }
+        if (summary[sumArri] == "YELLOW"){
+            sumArri+=1;
+            return setColor(arrYellow);
+        }
+        if (summary[sumArri] == "GREY"){
+            sumArri+=1;
+            return Color.GREY;
+        }
+        /*
+        if (sumArri > summary.length-1){
+            return Color.WHITE;
+        }
+*/
+    } else {
+        return Color.GREY;
+    }
+}
+
+function removeMenu() {
+    menuBool = false;
+    remove(menuText);
+    remove(normalButton);
+    remove(easyText);
+    remove(normalShadow);
+    //remove(hardButton);
+}
+
+function addStatsScreen() {
+    winScreenON = false;
+    statsScreenON = true;
+    add(statsBack);
+    drawTiles();
+}
+
+function click(e) {
+    if (menu) {
+        if (normalButton.containsPoint(e.getX(), e.getY())) {
+            removeMenu();
+            NUM_SQUARES = 6;
+            NUM_ROWS = 6;
+            game();
+        }
+        /*
+        if (hardButton.containsPoint(e.getX(), e.getY())){
+            removeMenu();
+            NUM_SQUARES = 8;
+            NUM_ROWS = 6;
+        }
+*/
+        music = new Audio("music.mp3");
+        music.play();
+        music.loop = true;
+    }
+    if (winScreenON) {
+        if (statsButton.containsPoint(e.getX(), e.getY())) {
+            addStatsScreen();
+        }
+    }
+}
+
+function drawLabel(text, color) {
     label.setText(text);
     label.setPosition(
         getWidth() / 2 - label.getWidth() / 2,
         getHeight() / 2 + label.getHeight() / 2
     );
+    label.setColor(color);
     add(label);
     return label;
 }
@@ -37,16 +235,18 @@ function setColor(arr) {
     return color;
 }
 
-function drawSquare(x, y, length, color) {
-    var square = new Rectangle(length, length);
+function drawSquare(x, y, length, width, color) {
+    var square = new Rectangle(length, width);
     square.setPosition(x, y);
     square.setColor(color);
     add(square);
 }
 
-function drawText(x, y, num) {
-    var text = new Text(num, FONT);
-    text.setPosition(x - text.getWidth() / 2, y + text.getHeight() / 2 - 3);
+function drawText(x, y, num, color, font) {
+    if (num == null) return;
+    var text = new Text(num, font);
+    text.setPosition(x - text.getWidth() / 2, y + text.getHeight() / 2 - 1);
+    text.setColor(color);
     add(text);
     return text;
 }
@@ -60,7 +260,7 @@ function backspace() {
 /* Validates the guess has NUM_SQUARES characters and good math */
 function isValidGuess() {
     var equation = opReplace(guess);
-    var valid = eval(equation);
+    valid = eval(equation);
     return guess.length == NUM_SQUARES && valid == true;
 }
 
@@ -82,19 +282,35 @@ function keyDown(e) {
     if (e.keyCode == Keyboard.ENTER) {
         if (isValidGuess()) {
             console.log("Final");
-            drawFinalGuess();
-            guess = "";
-            col++;
-
-            return;
-        } else {
-            if (guess.length == NUM_SQUARES){
-                var wrongLabel = drawLabel("Math is wrong!");
-                invalidGuess();
-                setTimeout(drawCurrentGuess, 1000);
-                setTimeout(function() { remove(wrongLabel); }, 1000);
+            guessCount += 1;
+            if (guess == answer/* || cheat == true*/) {
+                squareX = PADDING;
+                guess = "";
+                for (var i=0; i<NUM_SQUARES; i++){
+                    summary.push("GREEN");
+                }
+                animateSquares();
+                winScreenON = true;
+            } else {
+                drawFinalGuess();
+                guess = "";
+                row++;
                 return;
             }
+        } else {
+            incoBack = new Rectangle(getWidth(), getHeight());
+            incoBack.setPosition(0, 0);
+            incoBack.setColor(Color.WHITE);
+
+            invalidGuess();
+            setTimeout(drawCurrentGuess, 1000);
+            add(incoBack);
+            var wrongLabel = drawText(getWidth() / 2, getHeight() / 2 - 50, "Math is wrong!", Color.BLACK, FONT);
+            var wrongLabel2 = drawText(getWidth() / 2, getHeight() / 2 - 10, "Please enter a correct " + NUM_SQUARES + " digit answer", Color.BLACK, "12pt Monospace");
+            setTimeout(function() { remove(incoBack); }, 2500);
+            setTimeout(function() { remove(wrongLabel); }, 2500);
+            setTimeout(function() { remove(wrongLabel2); }, 2500);
+            return;
         }
     }
     // Backspace
@@ -156,61 +372,136 @@ function removeLabels() {
     curLabels = [];
 }
 
+function setSquareColor(x, y, color) {
+    var square = getElementAt(x, y);
+    if (square != null) {
+        square.setColor(color);
+    }
+}
+
 function drawCurrentGuess() {
     removeLabels();
-    var squareY = PADDING + col * (PADDING + length);
-    var squareX = PADDING;
+    squareX = PADDING;
     for (var i = 0; i < NUM_SQUARES; i++) {
-        drawSquare(squareX, squareY, length, Color.grey);
-        if(i < guess.length) {
+        setSquareColor(squareX, squareY, Color.grey);
+        if (i < guess.length) {
             var character = guess[i];
-            curLabels.push(drawText(squareX + length / 2, squareY + length / 2, character));
+            curLabels.push(drawText(squareX + length / 2, squareY + length / 2, character, Color.BLACK, FONT));
         }
         squareX += length + PADDING;
     }
 }
 
 function drawFinalGuess() {
-    var squareX = PADDING;
-    var squareY = PADDING + col * (PADDING + length);
+
+    /* FIX FOR YELLOW SQUARES */
+    let correctGuessMap = {
+        "0": 0,
+        "1": 0,
+        "2": 0,
+        "3": 0,
+        "4": 0,
+        "5": 0,
+        "6": 0,
+        "7": 0,
+        "8": 0,
+        "9": 0,
+        "+": 0,
+        "-": 0,
+        "x": 0,
+        "/": 0,
+        "=": 0,
+    }
+
     var green = setColor(arrGreen);
     var yellow = setColor(arrYellow);
+    squareX = PADDING;
+
     for (var i = 0; i < guess.length; i++) {
         var character = guess[i];
         if (character == answer[i]) {
-            drawSquare(squareX, squareY, length, green);
-            drawText(squareX + length / 2, squareY + length / 2, character);
+            correctGuessMap[character]++;
+            drawSquare(squareX, squareY, length, length, green);
+            drawText(squareX + length / 2, squareY + length / 2, character, Color.BLACK, FONT);
+            summary.push("GREEN");
         } else if (answer.includes(character)) {
-            drawSquare(squareX, squareY, length, yellow);
-            drawText(squareX + length / 2, squareY + length / 2, character);
+            drawSquare(squareX, squareY, length, length, yellow);
+            drawText(squareX + length / 2, squareY + length / 2, character, Color.BLACK, FONT);
+            summary.push("YELLOW");
         } else {
-            drawSquare(squareX, squareY, length, Color.grey);
-            drawText(squareX + length / 2, squareY + length / 2, character);
+            drawText(squareX + length / 2, squareY + length / 2, character, Color.BLACK, FONT);
+            summary.push("GREY");
         }
         squareX += length + PADDING;
     }
+    squareY += length + PADDING;
 }
 
 function invalidGuess() {
-    var squareX = PADDING;
-    var squareY = PADDING + col * (PADDING + length);
+    squareX = PADDING;
     var red = setColor(arrRed);
-    for (var i = 0; i < guess.length; i++) {
-        drawSquare(squareX, squareY, length, red);
-        drawText(squareX + length / 2, squareY + length / 2, guess[i]);
+    for (var i = 0; i < NUM_SQUARES; i++) {
+        setSquareColor(squareX, squareY, red);
         squareX += length + PADDING;
     }
 }
 
-function start() {
-    var squareY = PADDING;
-    for (var row = 0; row < NUM_ROWS; row++) {
-        var squareX = PADDING;
-        for (var col = 0; col < NUM_SQUARES; col++) {
-            drawSquare(squareX, squareY, length, Color.grey);
-            squareX += length + PADDING;
-        }
-        squareY += length + PADDING;
+function animateSquares() {
+    setTimer(newSquare, 500);
+}
+
+var x = 0;
+var growSquareX = PADDING + length / 2;
+
+function newSquare() {
+    if (x < NUM_SQUARES) {
+        setTimer(growSquare, 1);
+    } else {
+        stopTimer(newSquare);
+        addWinScreen();
+    }
+}
+
+function addWinScreen() {
+    add(blackscreen);
+    winText = drawLabel('You Win!', Color.red);
+    add(statsButton);
+    mouseClickMethod(click);
+}
+
+var side = 0;
+function growSquare() {
+    if (side <= length) {
+        drawSquare(growSquareX, squareY, side, side, setColor(arrGreen));
+        side++;
+    } else {
+        side = 0;
+        growSquareX += length + PADDING;
+        x += 1;
+        addAnswerText();
+    }
+}
+
+var i = 0;
+function addAnswerText() {
+    var winCharacter = answer[i];
+    drawText(growSquareX - PADDING - length / 2, squareY + length / 2, winCharacter, Color.BLACK, FONT);
+    i += 1;
+}
+
+
+function winCheat() {
+    cheat = true;
+    console.log("Cheater!");
+    console.log("> > > PRESS ENTER < < <");
+}
+
+function game() {
+    length = (width - PADDING) / NUM_SQUARES - PADDING;
+    drawTiles();
+    if (getAnswer) {
+        getAnswer = false;
+        answer = getRandomMathProblem(/*easy, hard*/);
     }
     label = new Text("");
     keyDownMethod(keyDown);
--- problems.js
+++ problems.js
@@ -1,16 +1,74 @@
-var problems = [
+
+function getRandomMathProblem() {
+    var equations = [
+        getAdditionProblem, getMultiplicationProblem, getDivisionProblem, getSubtractionProblem
+    ];
     
-];
-
-var addingNumbers = "";
-var random1 = Randomize.nextInt(0, 10);
-addingNumbers.concat(random1);
-addingNumbers.push("+");
-random2 = Randomize.nextInt(0, 10);
-while (random1 + random2 < 10){
-    random2 = Randomize.nextInt(0, 10);
+    return equations[(Math.floor(Math.random() * equations.length))]();
 }
-addingNumbers.push(random2);
-addingNumbers.push("=");
-addingNumbers.push(random1 + random2);
 
+function getAdditionProblem() {
+    var equation = "";
+    var random1 = Randomizer.nextInt(1, 9);
+    equation += random1;
+    equation += "+";
+    var random2 = Randomizer.nextInt(1, 9);
+    while (random1 + random2 < 10) {
+        random2 = Randomizer.nextInt(1, 9);
+    }
+    equation += random2;
+    equation += "=";
+    equation += (random1 + random2);
+    return equation;
+}
+
+function getMultiplicationProblem() {
+    var equation = "";
+    var random1 = Randomizer.nextInt(1, 9);
+    equation += random1;
+    equation += "x";
+    var random2 = Randomizer.nextInt(1, 9);
+    while (random1 * random2 < 10) {
+        random2 = Randomizer.nextInt(1, 9);
+    }
+    equation += random2;
+    equation += "=";
+    equation += (random1 * random2);
+    return equation;
+}
+
+function getDivisionProblem() {
+    var equation = "";
+    var random1 = Randomizer.nextInt(1, 9);
+    var random2 = Randomizer.nextInt(1, 9);
+    while (random1 * random2 < 10) {
+        random2 = Randomizer.nextInt(1, 9);
+    }
+    equation += (random1 * random2);
+    equation += "/";
+    equation += random1;
+    equation += "=";
+    equation += random2;
+    return equation;
+}
+
+function getSubtractionProblem() {
+    var equation = "";
+    var random1 = Randomizer.nextInt(1, 9);
+    var random2 = Randomizer.nextInt(1, 9);
+    while (random1 + random2 < 10) {
+        random2 = Randomizer.nextInt(1, 9);
+    }
+    equation += (random1 + random2);
+    equation += "-";
+    equation += random1;
+    equation += "=";
+    equation += random2;
+    return equation;
+}
+
+/*
+function getHardMathProblem() {
+  
+}
+*/
